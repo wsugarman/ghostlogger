91331aaf608deab46c382abde4cb959e915e7038
Will Sugarman <will.sugarman@microsoft.com>
2023-07-04 11:46:47 -0700

    Add .NET Standard Repository Template (#1)
    Reuse the various files from other `wsugarman` repositories when
    developing .NET Standard 2.0 libraries

---
+++ src/lang/IsExternalInit.cs
@@ -0,0 +1,17 @@
+// Copyright © William Sugarman.
+// Licensed under the MIT License.
+
+// This file defines the IsExternalInit static class used to implement init-only properties.
+//
+// Documentation: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/init
+// Source: https://github.com/dotnet/runtime/blob/v7.0.0/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/IsExternalInit.cs
+
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Runtime.CompilerServices;
+
+[ExcludeFromCodeCoverage]
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class IsExternalInit
+{ }
